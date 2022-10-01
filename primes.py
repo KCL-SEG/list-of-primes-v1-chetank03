9716cc71f6c806cf985d41e9e13e6493a28772eb
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-01 17:34:48 +0100

    Add comments to the primes.py

--- primes.py
+++ primes.py
@@ -3,17 +3,17 @@
 
 def primes(number_of_primes):
     list = []
-    count = 0
-    num = 2
-    while count < number_of_primes:
+    count = 0 #Keeps the count of for the numbers needed to be print.
+    num = 2 # Start with smallest prime and get incremented.
+    while count < number_of_primes:  # Keeps running till entered number of prime numbers are added to the list.
        
-        for i in range(2,(num//2)+1):
+        for i in range(2,(num//2)+1):  # Loops from 2 to (num//2)+1, to check the if the number is divisibility
             if num%i==0: 
-                break
-        else:
+                break 
+        else:               #If number is not divivsible, num is added to the list
             list.append(num);
-            count+=1
-        num+=1
+            count+=1  #increments the count
+        num+=1  #increments the number
 
     return list
 
