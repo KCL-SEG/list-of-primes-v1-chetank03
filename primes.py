301c4c893e2982ab83ede5af985bb03699aeec1a
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-01 17:26:25 +0100

    Add function to print first n prime number.

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    num = 2
+    while count < number_of_primes:
+       
+        for i in range(2,(num//2)+1):
+            if num%i==0: 
+                break
+        else:
+            list.append(num);
+            count+=1
+        num+=1
+
     return list
+
